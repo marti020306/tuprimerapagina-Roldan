89eb12df7863e95f8f8163752d009bf867992b32
Martina Roldan <laramartinaroldan@gmail.com>
2025-02-17 16:22:11 -0300

    forms python

---
+++ blog/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from .models import Autor, Categoria, Post
+
+
+class AutorForm(forms.ModelForm):
+    class Meta:
+        model = Autor
+        fields = ['nombre', 'apellido', 'email']
+
+
+class CategoriaForm(forms.ModelForm):
+    class Meta:
+        model = Categoria
+        fields = ['nombre']
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['titulo', 'contenido', 'autor', 'categoria']
+
+
+class BusquedaPostForm(forms.Form):
+    query = forms.CharField(label='Buscar Posts', max_length=100)
